2ea5774e7c0aadf36b83a402db5ce800ef7146c8
Akwinder kaur <kaura95@student.douglascollege.ca>
2020-10-02 23:00:59 -0700

    Branch

---
+++ incredible-Assignment2/src/main/java/com/csis3275/Model_Incredible_Asg2/Model_aka_06.java
@@ -0,0 +1,16 @@
+package com.csis3275.Model_Incredible_Asg2;
+
+public class Model_aka_06 extends Modelincredible_Asg2 {
+	
+	private String memberDescrip;
+
+	public String getMemberDescrip() {
+		return memberDescrip;
+	}
+
+	public void setMemberDescrip(String memberDescrip) {
+		this.memberDescrip = memberDescrip;
+	}
+	
+
+}
